212135f95a8758ff573c834e3fbe40074d2b6949
Gustavo Kazuo <kazucalado@gmail.com>
2021-11-02 22:39:39 -0300

    testevariavevl

---
+++ testevar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "nome do usuário $USER"
+echo "UID: $UID"
+echo "Diretorio home: $HOME"
---
+++ testevar2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# teste de variaveis
+idade=21
+nome="Gustavo"
+echo "O $nome tem $idade anos de idade"
